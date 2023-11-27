999004a0ab03b249101e2095f232692ad533caab
Maksym Mikhieiev <mih123eeev@gmail.com>
2023-11-27 23:57:29 +0200

    All functionality added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.madt_lab_5"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.madt_lab_5"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/madt_lab_5/Constant.java
@@ -0,0 +1,5 @@
+package com.example.madt_lab_5;
+
+public class Constant {
+    public static final String FLOATRATES_API_URL = "http://www.floatrates.com/daily/usd.xml";
+}
